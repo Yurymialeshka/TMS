e06cbced421a3b0ee297884c6050d49ca049b1a2
Yurymialeshka <energie85@mail.ru>
2022-02-18 14:58:18 +0300

    add Scanner

--- src/yurymialeshka/Main.java
+++ src/yurymialeshka/Main.java
@@ -1,8 +1,22 @@
 package yurymialeshka;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Будь так добр, введи слово <user>:");
+
+        String user = sc.next("<user>");
+
+        System.out.println("Hello " + user + " !");
+
+        sc.close();
     }
 }
+
