1dad77f899cd01c2eb647b1b5fccd3cd40e09646
Yurymialeshka <energie85@mail.ru>
2022-02-25 13:42:08 +0300

    Add Lesson3task8

---
+++ src/yurymialeshka/Lesson4task8.java
@@ -0,0 +1,17 @@
+package yurymialeshka;
+
+import java.util.Scanner;
+
+public class Lesson4task8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введи число: ");
+        int a = sc.nextInt();
+        System.out.println("Вы ввели: " + a);
+        int b = 0;
+        for (int i = 1; i <= a; i++) {
+            b = b + i;
+        }
+        System.out.println("Сумма от 1 до введенного с консоли равна: " + b);
+    }
+}
