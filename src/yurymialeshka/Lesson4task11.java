b2714272476770709052c48de8087a2012ad2f99
Yurymialeshka <energie85@mail.ru>
2022-02-27 14:35:35 +0300

    Add Lesson3task11

---
+++ src/yurymialeshka/Lesson4task11.java
@@ -0,0 +1,14 @@
+package yurymialeshka;
+
+public class Lesson4task11 {
+    public static void main(String[] args) {
+        int a = 9;
+        int b;
+        for (int i = 1; i <= 11; i++) {
+            a++;
+            b = a * a;
+
+            System.out.print(b + " ");
+        }
+    }
+}
