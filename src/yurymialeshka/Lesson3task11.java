b3beea614449871d8f5b9632de567f4c0af1e2fb
Yurymialeshka <energie85@mail.ru>
2022-02-27 14:47:47 +0300

    Add Lesson3task11

--- src/yurymialeshka/Lesson3task11.java
+++ src/yurymialeshka/Lesson3task11.java
@@ -2,13 +2,14 @@ package yurymialeshka;
 
 public class Lesson3task11 {
     public static void main(String[] args) {
-        int a = 9;
-        int b;
-        for (int i = 1; i <= 11; i++) {
-            a++;
-            b = a * a;
 
-            System.out.print(b + " ");
+        int a;
+
+        for (int i = 10; i <= 20; i++) {
+
+            a = i * i;
+
+            System.out.print(a + " ");
         }
     }
 }
