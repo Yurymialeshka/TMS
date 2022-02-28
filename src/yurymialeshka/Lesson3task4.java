7f2f0af67a196faea3804c6fec1479cce338acee
Yurymialeshka <energie85@mail.ru>
2022-02-28 21:07:04 +0300

    Lesson3task4 correct

--- src/yurymialeshka/Lesson3task4.java
+++ src/yurymialeshka/Lesson3task4.java
@@ -6,11 +6,11 @@ public class Lesson3task4 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите число:");
         int t = sc.nextInt();
-        if (t >= -4) {
+        if (t > -5) {
             System.out.println("Тепло");
         } else if (t <= -5 && t > -20) {
             System.out.println("Нормально");
-        } else {
+        } else if (t<=-20){
             System.out.println("Холодно");
         }
     }
