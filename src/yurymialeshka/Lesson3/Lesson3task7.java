b68748238346d0ac802cf86fbda4e6f95bdf0f8f
Yurymialeshka <energie85@mail.ru>
2022-04-09 15:48:58 +0400

    Create folder

---
+++ src/yurymialeshka/Lesson3/Lesson3task7.java
@@ -0,0 +1,9 @@
+package yurymialeshka;
+
+public class Lesson3task7 {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
---
+++ src/yurymialeshka/Lesson4/Lesson4.java
@@ -0,0 +1,37 @@
+package yurymialeshka;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Lesson4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        int attemps = 0;
+        int computerChoice = random.nextInt(100) + 1;
+        while (true) {
+            attemps++;
+            System.out.println("Количество попыток: " + attemps);
+            System.out.print("0- выход;Выберете число: ");
+
+            int num = sc.nextInt();
+
+            if (num == 0) {
+                System.out.println("На выход...");
+                break;
+            }
+            System.out.println("Вы выбрали:" + num);
+
+            if (computerChoice == num) {
+                System.out.println("Победа");
+                break;
+            } else if (computerChoice > num) {
+                System.out.println("Больше");
+                continue;
+            } else if (computerChoice < num) {
+                System.out.println("Меньше");
+                continue;
+            }
+        }
+    }
+}
