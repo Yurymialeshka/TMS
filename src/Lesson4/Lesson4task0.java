5b24faf6faae943d721d5da19976467895cb22de
Yurymialeshka <energie85@mail.ru>
2022-04-11 19:42:39 +0400

    Task1

---
+++ src/Lesson4/Lesson4task0.java
@@ -0,0 +1,34 @@
+package Lesson4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Lesson4task0 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] array = {6, 4, 9, 5, 7, 3};
+        System.out.println("Введите целое число: ");
+        int number = sc.nextInt();
+        Arrays.sort(array);
+        int a = Arrays.binarySearch(array, number);
+        if (a >= 0) {
+            System.out.println("Такое число входит в массив");
+        } else
+            System.out.println(" Такого числа нет в массиве");
+
+        boolean b = false;
+        for (int c : array) {
+            if (number == c) {
+                b = true;
+                System.out.println("Такое число входит в массив");
+                break;
+            }
+        }
+        if (b == false) {
+            System.out.println("Такого числа нет в массиве");
+        }
+    }
+}
+
+
+
