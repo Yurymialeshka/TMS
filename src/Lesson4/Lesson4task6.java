29d682f94fdd55b3d6e03892761b54b715a6a1b0
Yurymialeshka <energie85@mail.ru>
2022-04-14 06:12:19 +0400

    Lesson4task6 version 1.0

---
+++ src/Lesson4/Lesson4task6.java
@@ -0,0 +1,12 @@
+package Lesson4;
+
+import java.util.Arrays;
+
+public class Lesson4task6 {
+    public static void main(String[] args) {
+
+        String[] name = {"Petya", "Kolya", "Vasya", "German", "Ignat" };
+        Arrays.sort(name);
+        System.out.println(Arrays.toString(name));
+    }
+}
